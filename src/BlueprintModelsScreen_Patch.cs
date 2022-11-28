5513369c2a219668424b957ef79bbf09292f7742
NBKRedSpy <54865934+NBKRedSpy@users.noreply.github.com>
2022-11-28 17:59:02 -0500

    initial

---
+++ src/BlueprintModelsScreen_Patch.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+
+namespace DefaultBluePrintTab
+{
+    [HarmonyPatch(typeof(BlueprintModelsScreen), "ShowTab")]
+    public static class BlueprintModelsScreen_Patch
+    {
+        public static MethodInfo ShowSubTab = AccessTools.Method(typeof(BlueprintModelsScreen), "ShowSubTab", new Type[] { typeof(int) });
+
+        public static void Postfix(BlueprintModelsScreen __instance)
+        {
+            __instance.CurrentSubTab = -1;
+            ShowSubTab.Invoke(__instance, new object[] { -1 });
+        }
+
+    }
+}
---
+++ src/Plugin.cs
@@ -0,0 +1,27 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace DefaultBluePrintTab;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    public static ManualLogSource Log { get; set; }
+
+    private void Awake()
+    {
+
+        Log = Logger;
+
+        Harmony harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
+        harmony.PatchAll();
+
+    }
+
+    public static void LogInfo(string text)
+    {
+        Plugin.Log.LogInfo(text);
+    }
+
+}
